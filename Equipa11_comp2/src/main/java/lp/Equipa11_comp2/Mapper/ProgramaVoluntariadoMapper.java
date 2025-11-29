4525983dd3b3d0f1c97ec37e73a6b8b0cc5f4b94
miguel <imim18032000@gmail.com>
2025-11-29 20:01:53 +0000

    classes relacionadas a ProgramaVoluntariado

---
+++ Equipa11_comp2/src/main/java/lp/Equipa11_comp2/Mapper/ProgramaVoluntariadoMapper.java
@@ -0,0 +1,37 @@
+package lp.Equipa11_comp2.Mapper;
+
+/**
+ * @author miguel silva
+ */
+
+import org.springframework.stereotype.Component;
+import lp.Equipa11_comp2.DTO.ProgramaVoluntariadoDTO;
+import lp.Equipa11_comp2.Entity.ProgramaVoluntariado;
+
+@Component
+public class ProgramaVoluntariadoMapper {
+
+    public ProgramaVoluntariadoDTO toDTO(ProgramaVoluntariado p) {
+        return new ProgramaVoluntariadoDTO(
+            p.getId(),
+            p.getTitulo(),
+            p.getDescricao(),
+            p.getHorasServico(),
+            p.getLocal(),
+            p.getVagas(),
+            p.getVagasOcupadas()
+        );
+    }
+
+    public ProgramaVoluntariado toEntity(ProgramaVoluntariadoDTO dto) {
+        ProgramaVoluntariado p = new ProgramaVoluntariado(
+            dto.getTitulo(),
+            dto.getDescricao(),
+            dto.getHorasServico(),
+            dto.getLocal(),
+            dto.getVagas()
+        );
+        p.setVagasOcupadas(dto.getVagasOcupadas());
+        return p;
+    }
+}
---
+++ Equipa11_comp2/src/main/java/lp/Equipa11_comp2/Repository/ProgramaVoluntariadoRepository.java
@@ -0,0 +1,13 @@
+package lp.Equipa11_comp2.Repository;
+
+/**
+ * @author miguel silva
+ */
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import lp.Equipa11_comp2.Entity.ProgramaVoluntariado;
+
+@Repository
+public interface ProgramaVoluntariadoRepository extends JpaRepository<ProgramaVoluntariado, Long> {
+}
